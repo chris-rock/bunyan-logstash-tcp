8a75429f886c7f78841fa44e26728b8919241dc3
Christoph Hartmann <chris@lollyrock.com>
2014-07-06 13:55:48 +0200

    add example

---
+++ example/log.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var bunyan = require('bunyan'),
+    bunyantcp = require('../lib/logstash');
+
+var log = bunyan.createLogger({
+    name: 'example',
+    streams: [{
+        level: 'debug',
+        stream: process.stdout
+    },{
+        level: 'debug',
+        type: "raw",
+        stream: bunyantcp.createStream({
+            host: '127.0.0.1',
+            port: 9998
+        })
+    }],
+    level: 'debug'
+});
+
+log.debug('test');
+log.error('error test');
